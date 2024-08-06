80e5527264b5885c789d910f5183e47c039ad9fc
Baktiyar <bahtiyar_0194@mail.ru>
2024-08-06 14:37:59 +0500

    fourth commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const io = new Server(3001, {
     }
 });
 
-const peerServer = PeerServer({ port: 3002, path: '/peerjs/app' });
+const peerServer = PeerServer({ port: 3002, path: '/pee' });
 
 io.on('connection', (socket) => {
     socket.on('join-room', (roomId, userId) => {
