ca640787324cfb06c9f8db8d7734e12a47af15f5
Baktiyar <bahtiyar_0194@mail.ru>
2024-08-07 15:29:26 +0500

    seventeen commit

--- index.js
+++ index.js
@@ -1,49 +1,32 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const { ExpressPeerServer } = require('peer');
-const cors = require('cors');
+const { PeerServer } = require('peer');
+const { Server } = require("socket.io");
 
-const app = express();
-const server = http.createServer(app);
-
-// Настройка CORS
-app.use(cors({
-    origin: '*', // Укажите ваш фронтенд домен
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type'],
-    credentials: true
-}));
-
-const io = new Server(server, {
+const io = new Server(3001, {
     cors: {
-        origin: '*', // Укажите ваш фронтенд домен
-        methods: ['GET', 'POST'],
-        allowedHeaders: ['Content-Type'],
-        credentials: true
-    },
-    transports: ['websocket', 'polling']
-}); 
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 
-const peerServer = ExpressPeerServer(server, {
+const peerServer = PeerServer({ 
+    port: 3002, 
+    path: '/peerjs/myapp',
     debug: true
 });
 
-app.use('/peerjs/myapp', peerServer);
-
 io.on('connection', (socket) => {
-    console.log('a user connected:', socket.id);
-
     socket.on('join-room', (roomId, userId) => {
+        console.log(userId + ' connected');
         socket.join(roomId);
-        socket.to(roomId).emit('user-connected', userId);
+        socket.broadcast.to(roomId).emit('user-connected', userId);
 
-        socket.on('disconnect', () => {
-            socket.to(roomId).emit('user-disconnected', userId);
+        socket.on('user-disconnect', (roomId, userId) => {
+            console.log(userId + ' disconnected')
+            socket.broadcast.to(roomId).emit('user-disconnected', userId);
+        });
+    
+        socket.on('message', (roomId, data) => {
+            socket.broadcast.to(roomId).emit('new-message', data)
         });
     });
-});
-
-server.listen(3001, () => {
-    console.log('Server is running on port 3001');
 });
